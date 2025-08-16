cfba41af22c1020c7ffed1eab5d5689b55d4596c
Kumar Utkarsh <kumar.cs01032@gmail.com>
2025-08-16 10:03:35 +0530

    changed chat icon

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -5,7 +5,7 @@ const Chatbot = () => {
       chat-title="ZomatoChatbot"
       agent-id="6320b19e-9a1d-402e-80a6-f74f0454bbd5"
       language-code="en"
-      chat-icon="bot-4.png"
+      chat-icon="bot-5.png"
     ></df-messenger>
   );
 };
